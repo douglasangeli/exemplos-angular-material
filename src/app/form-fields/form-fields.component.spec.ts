76c2deb6f2a62bdef5373d83d8d13380dfb8a38b
Douglas Angeli <douglasangeli92@gmail.com>
2021-04-25 11:25:23 -0300

    chore: migrate to angular 11

--- src/app/form-fields/form-fields.component.spec.ts
+++ src/app/form-fields/form-fields.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FormFieldsComponent } from './form-fields.component';
 
@@ -6,7 +6,7 @@ describe('FormFieldsComponent', () => {
   let component: FormFieldsComponent;
   let fixture: ComponentFixture<FormFieldsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FormFieldsComponent ]
     })
--- src/app/inicial/inicial.component.spec.ts
+++ src/app/inicial/inicial.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InicialComponent } from './inicial.component';
 
@@ -6,7 +6,7 @@ describe('InicialComponent', () => {
   let component: InicialComponent;
   let fixture: ComponentFixture<InicialComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InicialComponent ]
     })
